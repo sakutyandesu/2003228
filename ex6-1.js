5e6c1e3d99626e389ace861fd6e118118a00ea28
sakutyandesu <131092105+sakutyandesu@users.noreply.github.com>
2023-10-17 10:22:28 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -6,5 +6,5 @@ function taro65(){who65='太郎';}
 function hanako65(){who65='花子';}
 
 function sayhello66(){alert('Hello,'+who66+'!');}
-function taro66(){alert(who66='太郎');}
-function hanako66(){alert(who66='花子');}
+function taro66(){alert(who66='太郎');alert(who66='太郎が来た');}
+function hanako66(){alert(who66='花子');alert(who66='花子が来た');}
