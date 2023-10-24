696f4b9688df0119a067a0ce776fd08cf84dc5c3
sakutyandesu <131092105+sakutyandesu@users.noreply.github.com>
2023-10-24 10:21:31 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,3 +1,3 @@
 function sayhello68(){alert('Hello,'+document.getElementByld('who68').innerHTML+'!');}
-function sayhello68(){alert(document.getElementByld('who68').innerHTML+'太郎');}
-function sayhello68(){alert(document.getElementByld('who68').innerHTML+'花子');}
+function sayhello68(){alert(document.getElementByld('who68').innerHTML='太郎');}
+function sayhello68(){alert(document.getElementByld('who68').innerHTML='花子');}
