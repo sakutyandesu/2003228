83a1b595696bb7ba22e9492d8b9aae6f72200649
sakutyandesu <131092105+sakutyandesu@users.noreply.github.com>
2023-11-14 09:53:41 +0900

    Create ex06-10.js

---
+++ ex06-10.js
@@ -0,0 +1,9 @@
+function imgchange1() {
+document.getElementById('logo').src = 
+'./image/nanamin1.png';
+}
+
+function imgchange2() {
+document.getElementById('logo').src = 
+'./image/nanamin2.png';
+}
