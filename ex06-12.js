2f9d7cbf312cbf8fcac4160dc688d29ad1531a9d
sakutyandesu <131092105+sakutyandesu@users.noreply.github.com>
2023-12-05 09:28:26 +0900

    Update ex06-12.js

--- ex06-12.js
+++ ex06-12.js
@@ -10,12 +10,12 @@ document.timer.moji.value=str.substr(0,cnt+1);}
 else {
 //全て表示されたら、空文字に戻す
 document.timer.moji.value="";}}
-function startfnc(){//関数hyoji()を1000ミリ秒間隔で呼び出す
+function startfnc(){
+//関数hyoji()を1000ミリ秒間隔で呼び出す
 setInterval("hyoji()",1000); }
 
 const str='Mozilla';
 console.log(str.substr(1,2));
 //expected output:"oz"
-
 console.log(str.substr(2));
 //expected output:"zilla"
