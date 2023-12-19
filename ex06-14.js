dee9ee102c49f385a6c98aeb7276aff15cf7373c
sakutyandesu <131092105+sakutyandesu@users.noreply.github.com>
2023-12-19 10:09:01 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,2 +1,2 @@
-function henkou1(){ document.getElementById('gh').src='./image/riza-donn.jpg';}
-function henkou2(){ document.getElementById('gh').src='./image/hitokage.jpg';}
+function henkou1(){ document.getElementById('gh').src='./image/haru.jpg';  setTimeout("henkou()",3000);}
+function henkou2(){ document.getElementById('gh').src='./image/yuki.jpg';}
