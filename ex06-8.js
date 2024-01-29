e3e3232656c115d4d3e1551273577a69c8f2f4f6
sakutyandesu <131092105+sakutyandesu@users.noreply.github.com>
2024-01-29 09:52:32 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,4 +1,7 @@
-function sayhello68(){alert('Hello,'+document.getElementByld('who68').innerHTML+'!');}
-function taro68(){document.getElementById('who68').innerHTML='太郎';}
-function hanako68(){document.getElementById('who68').innerHTML='花子';}
+function sayhello68() {alert('Hello,' + document.getElementById('who68').innerHTML + '!');}
+
+function taro68() {document.getElementById('who68').innerHTML = '太郎';}
+
+function hanako68() {document.getElementById('who68').innerHTML = '花子';}
+
 function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/nanamin1.jpg" alt="オン">';}
